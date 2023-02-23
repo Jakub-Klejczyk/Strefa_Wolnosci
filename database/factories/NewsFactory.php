67c4d3a24fb5294ec0f4e269d9f6986dcd22eaaa
Jakub <jakub-klejczyk@o2.pl>
2023-02-23 23:09:06 +0100

    first commit

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class NewsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => Str::random(10),
+            'text' => fake()->paragraph(5), 
+            'img' => Str::random(10), 
+        ];
+    }
+}
---
+++ resources/ts/App.vue
@@ -0,0 +1,5 @@
+<script script lang="ts"></script>
+
+<template>hi from vue</template>
+
+<style lang="scss" scoped></style>
